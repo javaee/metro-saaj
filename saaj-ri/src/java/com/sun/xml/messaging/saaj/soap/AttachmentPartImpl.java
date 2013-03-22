29acfba814086cce336dd869f947093b2e5f77c4
Gary Gordon <gary.gordon@oracle.com>
2013-03-22 12:42:58 +0000

    Implement hashCode() for JDK 8

--- saaj-ri/src/java/com/sun/xml/messaging/saaj/soap/AttachmentPartImpl.java
+++ saaj-ri/src/java/com/sun/xml/messaging/saaj/soap/AttachmentPartImpl.java
@@ -1,7 +1,7 @@
 /*
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
  *
- * Copyright (c) 1997-2012 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997-2013 Oracle and/or its affiliates. All rights reserved.
  *
  * The contents of this file are subject to the terms of either the GNU
  * General Public License Version 2 only ("GPL") or the Common Development
@@ -616,6 +616,12 @@ public class AttachmentPartImpl extends AttachmentPart {
     // attachments are equal if they are the same reference
     public boolean equals(Object o) {
         return (this == o);
+    }
+
+    // In JDK 8 we get a warning if we implement equals() but not hashCode().
+    // There is no intuitive value for this, the default one in Object is fine.
+    public int hashCode() {
+        return super.hashCode();
     }
 
     public MimeHeaders getMimeHeaders() {
